6bf9ee25736145bbcbdcc04f3ff38df4bd9ba962
hillaryodinson <insanedude.developer@gmail.com>
2026-04-26 21:59:54 +0100

    add static file serving and create index.html for the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { ScraperModule } from './scraper/scraper.module';
 import { NewsController } from './news/news.controller';
 import { NewsService } from './news/news.service';
 import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [ScraperModule, ScheduleModule.forRoot()],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    ScraperModule,
+    ScheduleModule.forRoot(),
+  ],
   controllers: [NewsController],
   providers: [NewsService],
 })
